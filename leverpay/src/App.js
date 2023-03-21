ed6e1bf879367ef154ff57c7af0c558923808205
Thomcray <ternathompson2@gmail.com>
2023-03-21 01:32:30 +0100

    modified src/App.js

--- leverpay/src/App.js
+++ leverpay/src/App.js
@@ -1,14 +1,28 @@
 //import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <p> Welcome! </p>
-        </div>
-      </header>
+      {/* <header className="App-header"> */}
+        <Container>
+          <Row>
+            <Col>
+              
+            </Col>
+            <Col xs={6}>
+              <h2> Welcome ! </h2>
+            </Col>
+            <Col>
+              
+            </Col>
+          </Row>
+        </Container>
+      {/* </header> */}
     </div>
   );
 }
