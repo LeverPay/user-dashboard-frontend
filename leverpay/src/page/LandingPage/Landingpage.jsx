f6c3ccf65ff44f348db7c5f596687afc96c956eb
Godswill Francis <willzudoh4@gmail.com>
2023-03-21 18:31:14 -0700

    created router for landing page and login page

---
+++ leverpay/src/page/LandingPage/Landingpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landingpage = () => {
+  return (
+    <div>
+      {/* navbar */}
+      {/* header */}
+      {/* overview */}
+      {/* ourservices */}
+      {/* productmarketing */}
+      {/* why we are best */}
+    </div>
+  );
+};
+
+export default Landingpage;
