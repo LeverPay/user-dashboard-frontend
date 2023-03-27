216b2f4625626763f8c16d62bf841ea9ec2011ff
Sarah <sarahnnanyelugo@gmail.com>
2023-03-27 15:03:01 +0100

    built the account upgrade forms built the modal for the account upgrade forms

--- leverpay/src/TestData/FileUploadData.js
+++ leverpay/src/TestData/FileUploadData.js
@@ -2,83 +2,88 @@ import Passport from "../assets/images/Passport.png";
 import London from "../assets/images/London-Cab.png";
 import ID from "../assets/images/id.png";
 
-export const gold = [
-  {
-    name: "Passport",
-    type: "file",
-    is_selected: false,
-    icon: Passport,
-    maxUpload: 2,
-  },
-  {
-    name: "International Passport",
-    type: "file",
-    is_selected: false,
-    icon: Passport,
-    maxUpload: 1,
-  },
-  {
-    name: "Driver License",
-    type: "file",
-    is_selected: false,
-    icon: London,
-    maxUpload: 2,
-  },
-  {
-    name: "Voters Card",
-    type: "file",
-    is_selected: false,
-    icon: ID,
-    maxUpload: 2,
-  },
-];
-
-export const inputPlaceholder = [
-  {
-    placeholder: "Residential Address",
-  },
-];
+export const gold = {
+  title: "Gold Account",
+  data: [
+    {
+      name: "Passport",
+      type: "file",
+      is_selected: false,
+      icon: Passport,
+      maxUpload: 2,
+    },
+    {
+      name: "International Passport",
+      type: "file",
+      is_selected: false,
+      icon: Passport,
+      maxUpload: 1,
+    },
+    {
+      name: "Driver License",
+      type: "file",
+      is_selected: false,
+      icon: London,
+      maxUpload: 2,
+    },
+    {
+      name: "Voters Card",
+      type: "file",
+      is_selected: false,
+      icon: ID,
+      maxUpload: 2,
+    },
+  ],
+  inputPlaceholder: [
+    {
+      placeholder: "Residential Address",
+    },
+  ],
+};
 
 ///diamond upgrade form
 
-export const diamond = [
-  {
-    name: "Passport",
-    type: "file",
-    is_selected: false,
-    icon: Passport,
-    maxUpload: 2,
-  },
-  {
-    name: "International Passport",
-    type: "file",
-    is_selected: false,
-    icon: Passport,
-    maxUpload: 1,
-  },
-  {
-    name: "Driver License",
-    type: "file",
-    is_selected: false,
-    icon: London,
-    maxUpload: 2,
-  },
-  {
-    name: "Voters Card",
-    type: "file",
-    is_selected: false,
-    icon: ID,
-    maxUpload: 2,
-  },
-];
-export const inputPlaceholderDiamond = [
-  {
-    placeholder: "NIN (National Identity Number)",
-  },
-  {
-    placeholder: "Utility Bill",
-  },
-  {
-    placeholder: "Residential Address",
-  },
-];
+export const diamond = {
+  title: "Diamond Account",
+  data: [
+    {
+      name: "Passport",
+      type: "file",
+      is_selected: false,
+      icon: Passport,
+      maxUpload: 2,
+    },
+    {
+      name: "International Passport",
+      type: "file",
+      is_selected: false,
+      icon: Passport,
+      maxUpload: 1,
+    },
+    {
+      name: "Driver License",
+      type: "file",
+      is_selected: false,
+      icon: London,
+      maxUpload: 2,
+    },
+    {
+      name: "Voters Card",
+      type: "file",
+      is_selected: false,
+      icon: ID,
+      maxUpload: 2,
+    },
+  ],
+  inputPlaceholder: [
+    {
+      placeholder: "NIN (National Identity Number)",
+    },
+    {
+      placeholder: "Utility Bill",
+    },
+    {
+      placeholder: "Residential Address",
+    },
+  ],
+};
