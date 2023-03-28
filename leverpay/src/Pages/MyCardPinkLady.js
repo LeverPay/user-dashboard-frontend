99059ef71071ef331d4fc74581085d15291deed0
S-Core21 <ogbonnashedrach8@gmail.com>
2023-03-28 21:52:43 +0100

    first commit

---
+++ leverpay/src/Pages/MyCardPinkLady.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Mycard from '../components/Mycard'
+import CardpinkLady from '../Cards/CardPinkLady'
+
+function MyCardPinkLady() {
+  return (
+    <Mycard card = {<CardpinkLady/>} />
+  )
+}
+
+export default MyCardPinkLady
