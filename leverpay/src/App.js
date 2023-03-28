48f19329101ff3cfc9de3d2289b1b8d90d50603a
S-Core21 <ogbonnashedrach8@gmail.com>
2023-03-28 21:52:43 +0100

    first commit

--- leverpay/src/App.js
+++ leverpay/src/App.js
@@ -1,30 +1,10 @@
-//import logo from './logo.svg';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import NavComponent from './components/shared-files/NavComponent';
-
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        <Container>
-          <Row>
-            <Col>
-              <NavComponent />
-            </Col>
-            <Col xs={6}>
-              <h2> Welcome ! </h2>
-            </Col>
-            <Col>
-              
-            </Col>
-          </Row>
-        </Container>
-      {/* </header> */}
     </div>
   );
 }
