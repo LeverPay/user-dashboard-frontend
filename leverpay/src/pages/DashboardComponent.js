bf74445b459d341674de70d589fd6ac36ce31fd7
Terna Nanev <ternathompson2@gmail.com>
2023-03-29 09:27:25 +0100

    modified cards component

--- leverpay/src/pages/DashboardComponent.js
+++ leverpay/src/pages/DashboardComponent.js
@@ -4,13 +4,37 @@ import TopNav from "../components/TopNav";
 import ShopDetailsComponent from "../components/ShopDetailsComponent";
 import CardsComponent from "../components/CardsComponent";
 
+import { naijaCardDetails, silverCardDetails } from "../CardData";
+import { useEffect, useState } from "react";
+
 const DashboardComponent = () => {
+  const [naijaCard, setNaijaCard] = useState({});
+  const [silverCard, setSilverCard] = useState({});
+
+  useEffect(() => {
+    setNaijaCard({
+      id: naijaCardDetails.map((data) => data.id),
+      cardHolder: naijaCardDetails.map((data) => data.cardHolder),
+      cardNo: naijaCardDetails.map((data) => data.cardNo),
+      expiryDate: naijaCardDetails.map((data) => data.expiryDate),
+    });
+  }, []);
+
+  useEffect(() => {
+    setSilverCard({
+      id: silverCardDetails.map((data) => data.id),
+      cardHolder: silverCardDetails.map((data) => data.cardHolder),
+      cardNo: silverCardDetails.map((data) => data.cardNo),
+      expiryDate: silverCardDetails.map((data) => data.expiryDate),
+    });
+  }, []);
+
   return (
     <>
       <TopNav />
       <StatementComponent />
       <RecentTransactionComponent />
-      <CardsComponent />
+      <CardsComponent naijaCardData={naijaCard} silverCardData={silverCard} />
       <ShopDetailsComponent />
     </>
   );
