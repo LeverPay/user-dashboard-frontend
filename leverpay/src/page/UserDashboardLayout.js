7fa9d84b1f13d03b1b12edce0dee51a7bd21e169
S-Core21 <ogbonnashedrach8@gmail.com>
2023-03-31 23:21:33 +0100

    modification

--- leverpay/src/page/UserDashboardLayout.js
+++ leverpay/src/page/UserDashboardLayout.js
@@ -8,10 +8,9 @@ import { useEffect, useState } from "react";
 import { naijaCardDetails, silverCardDetails } from "../TestData/CardData";
 import { Routes, Route } from "react-router-dom";
 import { Transactions } from "./Transactions/Transactions";
-// import InvoicePage from "../../Page/InvoicePage";
-import Mycard from "./MyCardPage.js/Mycard";
-import CardcategoryPage from "./CardCategoryPage/CardcategoryPage";
-import MyCardDiamond from "../page/DiamondCardPage/MyCardDiamond";
+import InvoicePage from "./InvoicePage/InvoicePage";
+import MyCardsSilver from "./SilverCardPage/MyCardsSilver";
+
 
 export const UserDashboardLayout = () => {
   const [naijaCard, setNaijaCard] = useState({});
@@ -67,9 +66,8 @@ export const UserDashboardLayout = () => {
               }
             />
             <Route path="transactions" element={<Transactions />} />
-            {/* <Route path="invoices" element={<InvoicePage />} /> */}
-            <Route path="invoices" element={<MyCardDiamond />} />
-            <Route path="my cards" element={<CardcategoryPage />} />
+            <Route path="invoices" element={<InvoicePage />} />
+            <Route path="my cards" element={<MyCardsSilver />} />
             {/* <Route path="settings" element />
         <Route path="*" element={<NoMatch />} /> */}
           </Routes>
