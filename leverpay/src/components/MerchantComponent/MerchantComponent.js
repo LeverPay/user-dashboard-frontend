e73bc79c33890ed9128ac7a24085b9c965b1b0f1
Sarah <sarahnnanyelugo@gmail.com>
2023-04-01 04:02:59 +0100

    built the merchant component

---
+++ leverpay/src/components/MerchantComponent/MerchantComponent.js
@@ -0,0 +1,45 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
+import "./MerchantComponent.css";
+import Merchants from "./Merchants/Merchants";
+import Apple from "../../assets/images/apple.png";
+import Apple2 from "../../assets/images/apple2.png";
+import Nike2 from "../../assets/images/nike2.png";
+import Nike from "../../assets/images/nike.png";
+import Netflix from "../../assets/images/netflix.png";
+import Netflix2 from "../../assets/images/netflix2.png";
+import Amazon from "../../assets/images/amazon.png";
+import Amazon2 from "../../assets/images/amazon2.png";
+export const MerchantComponent = () => {
+  return (
+    <>
+      <div className="col-md-12 merchant-header ">
+        <h3>My Merchants</h3>
+        <SearchBar />
+        <Link className="details-link up">View All</Link>
+      </div>
+      <div className="merchant-container col-md-12">
+        <div style={{ display: "flex" }}>
+          <h5>Shop Now</h5>
+          <Link className="details-link">More Details</Link>
+        </div>
+        <div className="merchants">
+          {" "}
+          <Merchants logo1={Apple} logo2={Apple2} />
+        </div>
+        <div className="merchants">
+          {" "}
+          <Merchants logo1={Nike2} logo2={Nike} />
+        </div>
+        <div className="merchants">
+          {" "}
+          <Merchants logo1={Netflix} logo2={Netflix2} />
+        </div>
+        <div className="merchants">
+          <Merchants logo1={Amazon} logo2={Amazon2} />
+        </div>
+      </div>
+    </>
+  );
+};
