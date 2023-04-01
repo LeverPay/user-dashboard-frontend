a7d8cc72f17bc2f3132a06ba3ac3bb31e6d7e359
S-Core21 <ogbonnashedrach8@gmail.com>
2023-04-01 20:22:16 +0100

    commit

--- leverpay/src/page/CardCategoryPage/CardCategory/Cardcategory.js
+++ leverpay/src/page/CardCategoryPage/CardCategory/Cardcategory.js
@@ -73,20 +73,30 @@ function Cardcategory() {
   return (
     <Container className="px-5 py-5 col-md-8 col-sm-9" id="cardcategories">
       {accReady ? <KYCFormModal acct={accSet} callback={defunct} /> : ""}
-      <Row>
+      
         <Row>
           <Col className="col-5" onClick={() => defunct("gold")}>
+            <Row>
             <CardGold />
+            </Row>
+            <Row className="mt-3">
+            <p className="Kyc-upgrade-notice" id="Kyc-upgrade-notice-1">
+              To upgrade to the <span className="gold">GOLD</span> Card, please
+              complete the following verification<strong> (KYC)</strong> Process
+              as it is required by Government. Click on the Card to start
+              Verification. Note :{" "}
+              <span className="limit">Daily Spending Limit is 1000 USDT</span>
+            </p>
+            </Row>
           </Col>
-          <Col className="col-2">
+          <Col className="col-2 pt-5 d-none d-md-block">
             <img alt="" className="img-responsive" src="./images/Line 25.png" />
           </Col>
           <Col className="col-5" onClick={() => defunct("diamond")}>
+            <Row>
             <CardDiamond />
-          </Col>
-        </Row>
-        <Row className="mt-2">
-          <Col className="align-items-center mx-3 ">
+            </Row>
+            <Row className="mt-3">
             <p className="Kyc-upgrade-notice" id="Kyc-upgrade-notice-1">
               To upgrade to the <span className="gold">GOLD</span> Card, please
               complete the following verification<strong> (KYC)</strong> Process
@@ -94,34 +104,19 @@ function Cardcategory() {
               Verification. Note :{" "}
               <span className="limit">Daily Spending Limit is 1000 USDT</span>
             </p>
-          </Col>
-          <Col className="col-2 "></Col>
-          <Col className="align-items-center ">
-            <p className="Kyc-upgrade-notice">
-              To upgrade to the <span className="gold">GOLD</span> Card, please
-              complete the following verification<strong> (KYC)</strong> Process
-              as it is required by Government. Click on the Card to start
-              Verification. Note :{" "}
-              <span className="limit">Daily Spending Limit is 1000 USDT</span>
-            </p>
-          </Col>
-        </Row>
-      </Row>
-      <Row className="mt-3">
-        <Row>
-          <Col className="col-5" onClick={() => defunct("pinkLady")}>
-            <CardPinkLady />
-          </Col>
-          <Col className="col-2">
-            <img alt="" className="img-responsive" src="./images/Line 25.png" />
-          </Col>
-          <Col className="col-5" onClick={() => defunct("enterprise")}>
-            <CardEnterprise />
+            </Row>
           </Col>
         </Row>
 
-        <Row className="mt-2">
-          <Col className="align-items-center mx-3">
+      
+      
+      <Row className="mt-5">
+        <Row>
+          <Col className="col-5" onClick={() => defunct("pinkLady")}>
+            <Row>
+            <CardPinkLady />
+            </Row>
+            <Row className="mt-3">
             <p className="Kyc-upgrade-notice" id="Kyc-upgrade-notice-1">
               To upgrade to the <span className="gold">GOLD</span> Card, please
               complete the following verification<strong> ( KYC )</strong>{" "}
@@ -129,18 +124,28 @@ function Cardcategory() {
               start Verification. Note :{" "}
               <span className="limit">Daily Spending Limit is 1000 USDT</span>
             </p>
+            </Row>
           </Col>
-          <Col className="col-2 "></Col>
-          <Col className="align-items-center ">
-            <p className="Kyc-upgrade-notice">
+          <Col className="col-2 pt-5 d-none d-md-block">
+            <img alt="" className="img-responsive" src="./images/Line 25.png" />
+          </Col>
+          <Col className="col-5" onClick={() => defunct("enterprise")}>
+            <Row>
+            <CardEnterprise />
+            </Row>
+            <Row className="mt-3">
+            <p className="Kyc-upgrade-notice" id="Kyc-upgrade-notice-1">
               To upgrade to the <span className="gold">GOLD</span> Card, please
               complete the following verification<strong> ( KYC )</strong>{" "}
               Process as it is required by Government. Click on the Card to
               start Verification. Note :{" "}
               <span className="limit">Daily Spending Limit is 1000 USDT</span>
             </p>
+            </Row>
           </Col>
         </Row>
+
+      
         {returnMessage && <ReturnMessage accountType={accSetInstance} />}
         {success && <SuccessMessage accountType={accSetInstance} />}
       </Row>
