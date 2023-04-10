df44dbce94f5bf8c7d134001daeb3fb5af722dcb
Terna Nanev <ternathompson2@gmail.com>
2023-04-10 12:45:46 +0100

    made profile-picture-change a component

--- leverpay/src/page/ProfilePage/ProfilePage.js
+++ leverpay/src/page/ProfilePage/ProfilePage.js
@@ -1,51 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../ProfilePage/ProfilePage.css";
-import avatar from "../../assets/images/avatar.png";
-import { MdOutlineEdit, MdDeleteOutline } from "react-icons/md";
-import Modal from "react-bootstrap/Modal";
 import Swal from "sweetalert2";
-// import { InputText } from "primereact/inputtext";
-import Avatar from "react-avatar-edit";
 import ImageComponent from "../../components/ImageComponent/ImageComponent";
-import { ErrorBoundary } from "react-error-boundary";
-
-export const SourceContext = React.createContext();
-export const AltContext = React.createContext();
-export const ClassContext = React.createContext();
 
 const ProfilePage = () => {
-  //   const [image, setImage] = useState("");
-  const [imagecrop, setImageCrop] = useState(false);
-  const [src, setSrc] = useState(false);
-  const [preview, setPreview] = useState(false);
-
-  const onClose = () => {
-    setPreview(false);
-  };
-
-  const onCrop = (view, e) => {
-    setPreview(view);
-  };
-
-  const onBeforeFileLoad = (e) => {
-    const file = e.target.files[0];
-    const type = file;
-
-    console.log(type.name);
-  };
-
-  const saveImageCrop = () => {
-    setPreview(preview);
-    setImageCrop(false);
-  };
-
-  const handleRemove = () => {
-    setPreview(!preview ? avatar : avatar);
-    setImageCrop(false);
-  };
-
   const saveChanges = (e) => {
     e.preventDefault();
 
@@ -90,80 +50,7 @@ const ProfilePage = () => {
         <h4>
           <b>Basic Information</b>
         </h4>
-        <div className="upload-control">
-          {/* Image Component  */}
-          <SourceContext.Provider value={preview ? preview : avatar}>
-            <AltContext.Provider value={""}>
-              <ClassContext.Provider value={"img-style"}>
-                <ImageComponent />
-              </ClassContext.Provider>
-            </AltContext.Provider>
-          </SourceContext.Provider>
-          {/* Image Component Ends Here */}
-          <Button
-            variant="primary"
-            className="change-img"
-            onClick={() => setImageCrop(true)}
-          >
-            Change <MdOutlineEdit />
-          </Button>
-
-          <Button variant="#fff" className="remove-img" onClick={handleRemove}>
-            Remove <MdDeleteOutline />
-          </Button>
-        </div>
-
-        {/* UPLOAD MODAL STARTS HERE */}
-        <Modal
-          show={imagecrop}
-          onHide={() => setImageCrop(false)}
-          backdrop="static"
-          keyboard={false}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Upload Profile Image</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {/* Avatar  */}
-            <ErrorBoundary
-              fallback={
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignContent: "center",
-                    backgroundColor: "red",
-                    color: "#fff",
-                  }}
-                >
-                  INVALID FILE FORMAT
-                </div>
-              }
-            >
-              <Avatar
-                width={"100%"}
-                height={300}
-                onCrop={onCrop}
-                onClose={onClose}
-                exportSize={390}
-                onBeforeFileLoad={onBeforeFileLoad}
-                src={src}
-              />
-            </ErrorBoundary>
-
-            {/* Body ends here  */}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => setImageCrop(false)}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={saveImageCrop}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        {/* UPLOAD MODAL ENDS HERE */}
-
+        <ImageComponent />
         {/* Form fields here */}
         <div className="fields-control">
           <Form.Group className="mb-3" controlId="">
