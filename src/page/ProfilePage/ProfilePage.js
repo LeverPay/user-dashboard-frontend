ad02ba889bba47d5201989ac0104c89718fd0fa1
Terna Nanev <ternathompson2@gmail.com>
2023-04-15 10:44:30 +0100

    TASK: add account form

--- src/page/ProfilePage/ProfilePage.js
+++ src/page/ProfilePage/ProfilePage.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../ProfilePage/ProfilePage.css";
 import Swal from "sweetalert2";
 import ImageComponent from "../../components/ImageComponent/ImageComponent";
+import AddAccountComponent from "../../components/AddAccountComponent/AddAccountComponent";
 
 const ProfilePage = () => {
+  const [phone, setPhone] = useState("");
+
+  const handlePhone = (e) => {
+    setPhone(e.target.value.replace(/\D/g, ""));
+  };
   const saveChanges = (e) => {
     e.preventDefault();
 
@@ -54,53 +60,58 @@ const ProfilePage = () => {
         {/* Form fields here */}
         <div className="fields-control">
           <Form.Group className="mb-3" controlId="">
-            <Form.Label>First Name</Form.Label>
+            <Form.Label className="labels">First Name</Form.Label>
             <Form.Control type="text" placeholder="" className="text-area" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="">
-            <Form.Label>Last Name</Form.Label>
+            <Form.Label className="labels">Last Name</Form.Label>
             <Form.Control type="text" placeholder="" className="text-area" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="">
-            <Form.Label>Business Name</Form.Label>
+            <Form.Label className="labels">Business Name</Form.Label>
             <Form.Control type="text" placeholder="" className="text-area" />
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Label>Country</Form.Label>
+            <Form.Label className="labels">Country</Form.Label>
             <Form.Select className="text-area" enabled>
               <option>Nigeria</option>
             </Form.Select>
           </Form.Group>
           <div className="col-md-12 phone-number-div">
-            <div className="col-md-3">
+            <div className="col-md-3 country-code-ctrl">
               <Form.Group className="mb-3">
                 <Form.Label className="phone-number-label">Code</Form.Label>
-                <Form.Select className="text-area country-code">
+                <Form.Select className="country-code">
                   <option>+234</option>
                 </Form.Select>
               </Form.Group>
             </div>
 
-            <div className="col-md-9">
+            <div className="col-md-9 phone-no-ctrl">
               <Form.Group className="mb-3">
-                <Form.Label>Phone Number</Form.Label>
+                <Form.Label className="labels">Phone Number</Form.Label>
                 <Form.Control
                   type="tel"
-                  placeholder=""
-                  pattern="[0-9]{10}"
-                  className="text-area phone-number"
+                  name="phone"
+                  value={phone}
+                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                  className="phone-number"
+                  onChange={handlePhone}
                 />
               </Form.Group>
             </div>
           </div>
-        </div>
-        <div className="button-control">
-          <Button variant="#fff" type="submit" onClick={discardChanges}>
-            Discard
-          </Button>
-          <Button variant="dark" type="submit" onClick={saveChanges}>
-            Save Changes
-          </Button>
+          {/* add account btn */}
+          <AddAccountComponent />
+          {/* end add account btn */}
+          <div className="button-control">
+            <Button variant="#fff" type="submit" onClick={discardChanges}>
+              Discard
+            </Button>
+            <Button variant="dark" type="submit" onClick={saveChanges}>
+              Save Changes
+            </Button>
+          </div>
         </div>
       </Form>
     </div>
