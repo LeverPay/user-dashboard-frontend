e08556b0eb9e9e4090a19deec97ec91dbe3363a9
S-Core21 <ogbonnashedrach8@gmail.com>
2023-05-06 07:48:46 +0100

    customer service support and feedback/help page commit

--- src/page/Transactions/TransactionTable/TransactionTable.js
+++ src/page/Transactions/TransactionTable/TransactionTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./transaction-table.css";
 import Table from "react-bootstrap/Table";
 import InvoiceModal from "../InvoiceModal/InvoiceModal";
+import { Link } from "react-router-dom";
 // import PrintComponent from "../InvoiceModal/InvoiceModal";
 // import { allTransactions } from "../../../TestData";
 
@@ -24,9 +25,12 @@ const TransactionTable = (props) => {
         <div className="table-title">
           <h5>{props.tableTitle}</h5>
           {/* <a href="/#">View all </a> */}
-          <a href="/#" style={{ color: "#fff", fontSize: "12px" }}>
-            Need help ?{" "}
-          </a>
+          <Link to='/customer-support'>
+          {" "}
+          <small style={{ color: "#fff", fontSize: "12px" }}>
+            Need help ?
+          </small>
+          </Link>
         </div>
         <table className="col-md-12 col-12">
           <thead>
