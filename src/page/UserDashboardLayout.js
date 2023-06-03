8abc42f050569e6b824fecebba5a0a391bbbf04a
Terna Nanev <ternathompson2@gmail.com>
2023-06-03 07:55:12 +0100

    modifications && api integration

--- src/page/UserDashboardLayout.js
+++ src/page/UserDashboardLayout.js
@@ -6,7 +6,7 @@ import TransactionTable from "./Transactions/TransactionTable/TransactionTable";
 import { recentTransactions } from "../TestData/TransactionsData.js";
 import { useEffect, useState } from "react";
 import { naijaCardDetails, silverCardDetails } from "../TestData/CardData";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import { Transactions } from "./Transactions/Transactions";
 import InvoicePage from "./InvoicePage/InvoicePage";
 // import Mycard from "./MyCardPage.js/Mycard";
@@ -23,10 +23,20 @@ import CardSilver from "../components/AllCards/CardSilver";
 import { MerchantComponent } from "../components/MerchantComponent/MerchantComponent";
 import "./UserDashboardLayout.css";
 import Settings from "./SettingsPage/Settings";
-import ProfilePage from "./ProfilePage/ProfilePage";
+import ProfilePage from "../page/ProfilePage/ProfilePage";
+import SignInPage from "./SignInPage/SignInPage";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import { useLocalState } from "../utils/useLocalStorage";
 export const UserDashboardLayout = () => {
   const [naijaCard, setNaijaCard] = useState({});
-  const [silverCard, setSilverCard] = useState({});
+  const [silverCard, setSilverCard] = useState([]);
+  const [user, setUser] = useState({});
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  // setTimeout(() => {
+  //   window.location.reload();
+  // }, 1000);
+
   useEffect(() => {
     setNaijaCard({
       id: naijaCardDetails.map((data) => data.id),
@@ -44,89 +54,116 @@ export const UserDashboardLayout = () => {
       expiryDate: silverCardDetails.map((data) => data.expiryDate),
     });
   }, []);
+  // useEffect(() => {
+  //   const reloadCount = sessionStorage.getItem("reloadCount");
+  //   if (reloadCount < 2) {
+  //     sessionStorage.setItem("reloadCount", String(reloadCount + 1));
+
+  //     // window.location.reload();
+  //   } else {
+  //     sessionStorage.removeItem("reloadCount");
+  //   }
+  // });
+
+  // useEffect(() => {
+  //   // window.location.reload();
+  //   setUser({
+  //     userName: Object.values(jwt).map((data) => data.user.first_name),
+  //   });
+  // }, [user, jwt]);
+
   return (
     <>
-      <div className="col-md-12 flexy" style={{ overflowX: "hidden" }}>
-        <div className="col-md-2">
-          <NavComponent />
-        </div>
-        <div className="col-md-10">
-          <TopNav />
-
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <div
-                  className="col-md-12 flexy"
-                  style={{ marginTop: "8.5rem" }}
-                >
-                  <div className="col-md-8">
-                    <div
-                      className="col-md-12 flexy"
-                      style={{ marginTop: "-1rem" }}
-                    >
-                      <div className="col-md-4">
-                        {" "}
-                        <TotalMoney
-                          bg="#0E093F"
-                          totaltype="Balance"
-                          amt="$3000"
-                        />
-                      </div>{" "}
-                      <div className="col-md-4">
-                        {" "}
-                        <TotalMoney
-                          bg="#F6A61F"
-                          totaltype="Spending"
-                          amt="$2000"
-                        />
-                      </div>{" "}
-                      <div className="col-md-4">
-                        {" "}
-                        <TotalMoney bg="#201E34" totaltype="Saved" amt="$546" />
-                      </div>
-                    </div>
-                    <div className="col-md-11">
+      <Routes>
+        <Route
+          element={
+            <>
+              <div className="col-md-12 flexy" style={{ overflowX: "hidden" }}>
+                <div className="col-md-2">
+                  <NavComponent />
+                </div>
+                <div className="col-md-10">
+                  <TopNav userName={user.userName} />
+                  <PrivateRoute>
+                    <Outlet />
+                  </PrivateRoute>
+                </div>
+              </div>
+            </>
+          }
+        >
+          <Route
+            path="/"
+            element={
+              <div className="col-md-12 flexy" style={{ marginTop: "8.5rem" }}>
+                <div className="col-md-8">
+                  <div
+                    className="col-md-12 flexy"
+                    style={{ marginTop: "-1rem" }}
+                  >
+                    <div className="col-md-4">
                       {" "}
-                      <StatementComponent />
-                    </div>
-                    <div className="transaction-table-container col-md-11">
-                      <TransactionTable
-                        data={recentTransactions}
-                        tableTitle="Recent Transaction"
+                      <TotalMoney
+                        bg="#0E093F"
+                        totaltype="Balance"
+                        amt="$3000"
                       />
+                    </div>{" "}
+                    <div className="col-md-4">
+                      {" "}
+                      <TotalMoney
+                        bg="#F6A61F"
+                        totaltype="Spending"
+                        amt="$2000"
+                      />
+                    </div>{" "}
+                    <div className="col-md-4">
+                      {" "}
+                      <TotalMoney bg="#201E34" totaltype="Saved" amt="$546" />
                     </div>
                   </div>
-                  <div className="col-md-4  card-holder">
-                    <div className="col-md-10 mx-auto default-card-holder">
-                      <header className="card-header">My Card</header>
-                      <CardUser />
-                    </div>{" "}
-                    <div
-                      className="col-md-10 mx-auto"
-                      style={{ transform: "translateY(-3.5rem)" }}
-                    >
-                      <CardSilver />
-                    </div>
-                    <div className="col-md-12">
-                      <MerchantComponent />
-                    </div>
+                  <div className="col-md-11">
+                    {" "}
+                    <StatementComponent />
+                  </div>
+                  <div className="transaction-table-container col-md-11">
+                    <TransactionTable
+                      data={recentTransactions}
+                      tableTitle="Recent Transaction"
+                    />
                   </div>
                 </div>
-              }
-            />
-            <Route path="transactions" element={<Transactions />} />
-            <Route path="invoices" element={<InvoicePage />} />
-            <Route path="cardCategories" element={<CardcategoryPage />} />
-            <Route path="account" element={<MyUpgradedAccount />} />
-            <Route path="my cards" element={<MyCardsSilver />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="profile" element={<ProfilePage />} />
-            {/* <Route path="*" element={<NoMatch />} /> */}
-          </Routes>
-        </div>
-      </div>
+                <div className="col-md-4  card-holder">
+                  <div className="col-md-10 mx-auto default-card-holder">
+                    <header className="card-header">My Card</header>
+                    <CardUser />
+                  </div>{" "}
+                  <div
+                    className="col-md-10 mx-auto"
+                    style={{ transform: "translateY(-3.5rem)" }}
+                  >
+                    <CardSilver />
+                  </div>
+                  <div className="col-md-12">
+                    <MerchantComponent />
+                  </div>
+                </div>
+              </div>
+            }
+          />
+
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="invoices" element={<InvoicePage />} />
+          <Route path="cardCategories" element={<CardcategoryPage />} />
+          <Route path="account" element={<MyUpgradedAccount />} />
+          <Route path="my cards" element={<MyCardsSilver />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
+        <Route path="signin" element={<SignInPage />} />
+      </Routes>
+      {/* </div> */}
+      {/* </div> */}
     </>
   );
 };
