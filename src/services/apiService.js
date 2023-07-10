7a5da6a6fe8d0d7d27d694a1d2a5ab89a7dcb37b
Terna Nanev <ternathompson2@gmail.com>
2023-07-10 16:09:19 +0100

    IMPL. forgot password IMPL. reset password

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,5 @@
 import { toast } from "react-toastify";
+// import ResetPassword from "../components/ResetPasswordComponent/ResetPassword";
 // const URL = "http://api.leverpay.io/api/v1/user/update-user-profile";
 
 export const signIn = async (userData, jwt, setJwt) => {
@@ -16,7 +17,7 @@ export const signIn = async (userData, jwt, setJwt) => {
       .then((res) => {
         if (res.success) {
           toast.success(`${res.message}`);
-          console.log(res);
+          // console.log(res);
           setJwt(`${res.data.token}`);
           //transition to homepage
           setTimeout(() => {
@@ -24,7 +25,7 @@ export const signIn = async (userData, jwt, setJwt) => {
           }, 2000);
         } else {
           toast.error(`${res.message}`);
-          console.log(res);
+          // console.log(res);
         }
       })
       .catch((err) => {
@@ -52,10 +53,10 @@ export const getUserProfile = async (jwt, setUser) => {
     })
     .then((resData) => {
       setUser(resData.data);
-      toast.success(resData.message);
+      // toast.success(resData.message);
     })
     .catch((err) => {
-      console.log(`${err.message}`);
+      console.log(`${err.data}`);
     });
 
   return await userProfile;
@@ -95,20 +96,50 @@ export const updateUserProfile = async (jwt, userDataUpdate) => {
   return await updateRes;
 };
 
+export const userResetPassword = async (passwordReset) => {
+  const resetPass = await fetch(
+    "https://api.leverpay.io/api/v1/reset-password",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(passwordReset),
+    }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.success !== false) {
+        toast.success(res);
+        setTimeout(() => {
+          window.location.href = "/signin";
+        }, 5000);
+      }
+      if (res.message === "Error") {
+        toast.error(`${res.data.new_password}`);
+      } else {
+        toast.error(res.message);
+      }
+    })
+    .catch((err) => {
+      toast.error(`${err}`);
+    });
+
+  return resetPass;
+};
+
 export const logoutUser = async (jwt) => {
   const logOut = await fetch("https://api.leverpay.io/api/v1/user/logout", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      // "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "*",
       Authorization: `Bearer ${jwt}`,
     },
   })
     .then((logoutResponse) => {
-      if (logoutResponse.status === 200) {
-        alert(logoutResponse);
-        return logoutResponse.json();
-      }
+      if (logoutResponse.status === 200) return logoutResponse.json();
     })
     .then((logoutData) => {
       toast.success(logoutData.message);
