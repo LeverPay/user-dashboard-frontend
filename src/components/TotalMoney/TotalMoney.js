4efd92e928c448e9725aa69ce6e8909c2c1dd2c5
Terna Nanev <ternathompson2@gmail.com>
2023-07-22 00:01:34 +0100

    transfer page

--- src/components/TotalMoney/TotalMoney.js
+++ src/components/TotalMoney/TotalMoney.js
@@ -4,20 +4,24 @@ import "./totalMoney.css";
 
 function TotalMoney(props) {
   const { bg, totaltype, amt } = props;
-  const [amtVisible, setAmtVisible] = useState(false)
+  const [amtVisible, setAmtVisible] = useState(false);
 
-  function viewamt(){
-    setAmtVisible(!amtVisible)
+  function viewamt() {
+    setAmtVisible(!amtVisible);
   }
 
   return (
-    <div className="card-balance col-md-12" style={{ backgroundColor: bg }} onClick={viewamt}>
+    <div
+      className="card-balance col-md-12"
+      style={{ backgroundColor: bg }}
+      onClick={viewamt}
+    >
       <span>
         <img alt="" src="./images/bal1.png" />
       </span>
       <main>
         <small>Total {totaltype} </small>
-        <h4> {amtVisible ? amt : 'XXXX'} </h4>
+        <h4> {amtVisible ? amt : "XXXX"} </h4>
       </main>
     </div>
   );
