33431fa62839c51a801db5dd1841a50edf206072
Terna Nanev <ternathompson2@gmail.com>
2023-08-03 07:18:04 +0100

    minor fix

--- src/page/FundingPage/FundingPage.js
+++ src/page/FundingPage/FundingPage.js
@@ -1,44 +1,49 @@
-import React, { useState } from 'react'
-import './FundingPage.css'
-import FundingNav from '../../components/AccountFunding/FundingNav'
-import { FundingPrices } from '../../components/AccountFunding/FundingPrices'
-import FundingNav2 from '../../components/AccountFunding/FundingNav2'
-import FundingPayment from '../../components/AccountFunding/FundingPayment'
-import { Outlet,Route,Routes } from 'react-router-dom'
-import FundingNaira from '../../components/AccountFunding/FundingNaira'
-import FundingPaystack from '../../components/AccountFunding/FundingPaystack'
-import FundingMore from '../../components/AccountFunding/FundingMore'
+import React, { useState } from "react";
+import "./FundingPage.css";
+import FundingNav from "../../components/AccountFunding/FundingNav";
+import { FundingPrices } from "../../components/AccountFunding/FundingPrices";
+import FundingNav2 from "../../components/AccountFunding/FundingNav2";
+import FundingPayment from "../../components/AccountFunding/FundingPayment";
+import { Outlet, Route, Routes } from "react-router-dom";
+import FundingNaira from "../../components/AccountFunding/FundingNaira";
+import FundingPaystack from "../../components/AccountFunding/FundingPaystack";
+import FundingMore from "../../components/AccountFunding/FundingMore";
 
 const FundingPage = () => {
-    const [amt, setAmt] = useState('')
-    function handleAmount(amts){
-        setAmt(amts)
-    }
+  const [amt, setAmt] = useState("");
+  function handleAmount(amts) {
+    setAmt(amts);
+  }
 
   return (
-    <div className='Fundingpage'>
-        <section>
-            <FundingNav/>
-        </section>
-        <section>
-            <FundingPrices amt = {amt} />
-        </section>
-        <section>
-            <FundingNav2/>
-        </section>
-        <section>
-            {/* <Outlet amts = {amt} /> */}
-            <Routes>
-            <Route index element={<FundingPayment/>} />
-          <Route path="stablecoins-deposit" element={<FundingPayment handleAmount = {handleAmount}  />} />
+    <div className="Fundingpage">
+      <section>
+        <FundingNav />
+      </section>
+      <section>
+        <FundingPrices amt={amt} />
+      </section>
+      <section>
+        <FundingNav2 />
+      </section>
+      <section>
+        {/* <Outlet amts = {amt} /> */}
+        <Routes>
+          <Route
+            index
+            element={<FundingPayment handleAmount={handleAmount} />}
+          />
+          <Route
+            path="stablecoins-deposit"
+            element={<FundingPayment handleAmount={handleAmount} />}
+          />
           <Route path="naira-deposit" element={<FundingNaira />} />
           <Route path="paystack-deposit" element={<FundingPaystack />} />
           <Route path="more" element={<FundingMore />} />
-            </Routes>
-        </section>
-
+        </Routes>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default FundingPage
+export default FundingPage;
