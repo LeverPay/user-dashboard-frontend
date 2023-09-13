9ba6f6a568df2f1abafe960239e0d9465552ae47
S-Core21 <ogbonnashedrach8@gmail.com>
2023-09-13 01:36:27 +0100

    investment page responsive

--- src/components/CheckoutTransfer/CheckoutTransfer.js
+++ src/components/CheckoutTransfer/CheckoutTransfer.js
@@ -5,8 +5,11 @@ import Helpimageupload from '../HelpImageUpload/helpimageupload'
 
 
 
-const CheckoutTransfer = () => {
+const CheckoutTransfer = ({amount, checkoutData, vat, txfee, prevStep, nextStep}) => {
+    const totalAmt = (amount * 1) + (vat*1) + (txfee*1)
     const [fileImg, setFileimg] = useState()
+    const [copyAlert, setCopyAlert] = useState('')
+    const [ncopyAlert, setNCopyAlert] = useState('')
     const [imgfile, setImgfile] = useState()
     function GetImg(value, imgInfo) {
         setFileimg(value)
@@ -23,7 +26,7 @@ const CheckoutTransfer = () => {
     const [account_no, setAccount_no] = useState(account_Info[0].number)
 
     const [formData, setFormData] = useState({
-        narration: '',
+        narration: 'leverpay20752',
         Bank_name: account_Info[0].name,
         fileImg: fileImg,
         imginfo: imgfile,
@@ -47,13 +50,41 @@ const CheckoutTransfer = () => {
       } else if (formData.Bank_name === account_Info[3].name) {
         return setAccount_no(account_Info[3].number)
       }
-    },[formData.Bank_name, account_Info])
+    },[formData.Bank_name])
+
+    useEffect(()=>{
+        checkoutData(formData)
+    },[formData])
+
+    function copyAcct() {
+        navigator.clipboard.writeText(account_no);
+        setCopyAlert('Copied')
+      }
+
+    function copyNarration (){
+        navigator.clipboard.writeText(formData.narration);
+        setNCopyAlert('Copied')
+    }
+
+    useEffect(()=>{
+        setCopyAlert('')
+        setNCopyAlert('')
+    }, [])
 
     return (
         <div className='checkoutTransfer'>
             <section className='acct'>
-                <label>
+                <label htmlFor='narration'>
                     Narration Code
+                    <span className='reminder'>
+                    Please use this code as your narration when making the transfer
+                    </span>
+                    <span className='narratCopy' onClick={copyNarration} >
+                        <img alt='copy' src='/images/copy.png' />
+                    </span>
+                    <span className='narratCopy' id='cpyNalrt' >
+                        {ncopyAlert}
+                    </span>
                 </label>
                 <input
                     type='text'
@@ -65,7 +96,6 @@ const CheckoutTransfer = () => {
 
                 <main>
                     <div className='FundingNairaBank'>
-                        <h1>Transfer NGN 5,000 to your Account</h1>
                         <main>
                             <div><h3>Account Name: </h3>  <h4>Leverchain Technology Limited</h4></div>
                             <div><h3>Bank Name </h3>
@@ -88,15 +118,15 @@ const CheckoutTransfer = () => {
 
                                 </select>
                             </div>
-                            <div><h3>Account Number: </h3>  <h4>{account_no}</h4><img alt='copy'  src='/images/acct_cpy.png' /></div>
-                            <div><h3>Amount: </h3>  <h4>NGN 5,000</h4></div>
+                            <div><h3>Account Number: </h3>  <h4>{account_no}</h4><img alt='copy' onClick={copyAcct} src='/images/acct_cpy.png' />{copyAlert}</div>
+                            <div><h3>Amount: </h3>  <h4>NGN {totalAmt}</h4></div>
                             <div className="screenshot">
                             <Helpimageupload GetfileImg={GetImg} optional={false} />
                         </div>
                         </main>
                     </div>
                 </main>
-                <button className='ChckTrfSub'>
+                <button className='ChckTrfSub' onClick={nextStep} > 
                     Payment Completed
                 </button>
             </section>
@@ -114,21 +144,21 @@ const CheckoutTransfer = () => {
                             Product <strong>Shares</strong>
                         </li>
                         <li>
-                            Invest Amount <strong>NGN 5,000</strong>
+                            Invest Amount <strong>NGN {amount}</strong>
                         </li>
                         <li>
-                            VAT <strong>NGN 375</strong>
+                            VAT <strong>NGN {vat}</strong>
                         </li>
                         <li>
-                            Transaction fee (1.3%) <strong>NGN 69.9</strong>
+                            Transaction fee (1.3%) <strong>NGN {txfee}</strong>
                         </li>
                     </ul>
                 </div>
                     <div className='payAmt'>
                         <h2>You have to pay : </h2>
-                        <h1><span>NGN</span> 5,400</h1>
+                        <h1><span>NGN</span> {totalAmt}</h1>
                     </div>
-                    <div className='bckOver'>
+                    <div className='bckOver' onClick={prevStep} >
                        <img alt='arr' src='/images/bckarr.png' /> Back to order overview
                     </div>
             </section>
