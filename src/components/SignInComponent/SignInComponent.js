9e299b4b05cef5d2b658c0b4f6ba1eb648e64474
Michael Oshosanya <michael.oshosanya@deimos.co.za>
2024-06-06 14:35:53 +0100

    debug

--- src/components/SignInComponent/SignInComponent.js
+++ src/components/SignInComponent/SignInComponent.js
@@ -17,101 +17,127 @@ import { useNavigate } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 
 const SignInComponent = () => {
-  const inputRef = React.createRef();
+    const inputRef = React.createRef();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [submitted, setSubmitted] = useState(false)
-  const [isvisible, setIsvisible] = useState(false)
-  const [jwt, setJwt] = useLocalState("", "jwt");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+    const [isvisible, setIsvisible] = useState(false);
+    const [jwt, setJwt] = useLocalState("", "jwt");
 
-  function toggleVisible(){
-    setIsvisible(!isvisible)
-  }
+    console.log(process.env.REACT_APP_LEVERPAY_API_URL);
 
-  const login = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const user = Object.fromEntries(formData);
+    function toggleVisible() {
+        setIsvisible(!isvisible);
+    }
 
-    //api call
-    signIn(user, jwt, setJwt);
-    setSubmitted(true)
-  };
+    const login = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const user = Object.fromEntries(formData);
 
-  const handleForgetPassword = () => {
-    forgotPassword();
-  };
+        //api call
+        signIn(user, jwt, setJwt);
+        setSubmitted(true);
+    };
 
-  useEffect(() => {
-    //console.log(`JWT is: ${jwt}`);
-  }, [jwt]);
+    const handleForgetPassword = () => {
+        forgotPassword();
+    };
 
-  return (
-    <div className="signin-container">
-      <div className="images-container">
-        <img src={LeverpayLogo} alt="" className="signin-logo" />
-        <img src={SignInImage} alt="" className="signin-image" />
-      </div>
-      <div className="form-container">
-        <p>
-          Don't have an account?{" "}
-          <span
-            className="signup-link"
-            onClick={() => (window.location.href = "/leverpay-signup")}
-          >
-            Create account
-          </span>
-        </p>
-        <Form className="signin-form" onSubmit={login}>
-          <h1 className="welcome-text">Welcome Back</h1>
-          <Form.Group className="mb-3 sign-in-email" controlId="formGroupEmail">
-            <Form.Control
-              type="email"
-              ref={inputRef}
-              value={email}
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder=""
-              className="signin-fields"
-              required
-            />
-            <img src={EmailIcon} alt="" className="input-icon" />
-          </Form.Group>
-          <Form.Group className="sign-in-pwd mb-3" controlId="formGroupPassword">
-            <Form.Control
-              type={isvisible ? 'text' : "password"}
-              ref={inputRef}
-              value={password}
-              name="password"
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder=""
-              className="signin-fields"
-              required
-            />
-            <img src={PasswordIcon} alt="" className="input-icon" />
-            <img alt="" src={isvisible ? "/images/blind-light.png" : "/images/visible-light.png"} onClick={toggleVisible} className="visible-blind" />
-          </Form.Group>
+    useEffect(() => {
+        //console.log(`JWT is: ${jwt}`);
+    }, [jwt]);
 
-          <Button variant="primary" type="submit" className="signin-button">
-            {submitted ? 'Loading... please wait':'Login'}
-          </Button>
-          <p className="forgot-password-link" onClick={handleForgetPassword}>
-            Forgot Password?
-          </p>
-        </Form>
+    return (
+        <div className="signin-container">
+            <div className="images-container">
+                <img src={LeverpayLogo} alt="" className="signin-logo" />
+                <img src={SignInImage} alt="" className="signin-image" />
+            </div>
+            <div className="form-container">
+                <p>
+                    Don't have an account?{" "}
+                    <span
+                        className="signup-link"
+                        onClick={() =>
+                            (window.location.href = "/leverpay-signup")
+                        }
+                    >
+                        Create account
+                    </span>
+                </p>
+                <Form className="signin-form" onSubmit={login}>
+                    <h1 className="welcome-text">Welcome Back</h1>
+                    <Form.Group
+                        className="mb-3 sign-in-email"
+                        controlId="formGroupEmail"
+                    >
+                        <Form.Control
+                            type="email"
+                            ref={inputRef}
+                            value={email}
+                            name="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder=""
+                            className="signin-fields"
+                            required
+                        />
+                        <img src={EmailIcon} alt="" className="input-icon" />
+                    </Form.Group>
+                    <Form.Group
+                        className="sign-in-pwd mb-3"
+                        controlId="formGroupPassword"
+                    >
+                        <Form.Control
+                            type={isvisible ? "text" : "password"}
+                            ref={inputRef}
+                            value={password}
+                            name="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder=""
+                            className="signin-fields"
+                            required
+                        />
+                        <img src={PasswordIcon} alt="" className="input-icon" />
+                        <img
+                            alt=""
+                            src={
+                                isvisible
+                                    ? "/images/blind-light.png"
+                                    : "/images/visible-light.png"
+                            }
+                            onClick={toggleVisible}
+                            className="visible-blind"
+                        />
+                    </Form.Group>
 
-        <div className="terms">
-          <li>Privacy & Terms</li>
-          <li>Contact Us</li>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        className="signin-button"
+                    >
+                        {submitted ? "Loading... please wait" : "Login"}
+                    </Button>
+                    <p
+                        className="forgot-password-link"
+                        onClick={handleForgetPassword}
+                    >
+                        Forgot Password?
+                    </p>
+                </Form>
+
+                <div className="terms">
+                    <li>Privacy & Terms</li>
+                    <li>Contact Us</li>
+                </div>
+
+                <img src={Blopp} alt="" className="blop" />
+
+                <ToastContainer />
+            </div>
         </div>
-
-        <img src={Blopp} alt="" className="blop"/>
-      
-      <ToastContainer />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SignInComponent;
