a3f0801cb718a0eea3f75d5737a3efa2e95ae965
KevinEjiofor <ejioforkelvin@gmail.com>
2024-06-18 13:44:30 +0100

    updating paybill design

---
+++ src/components/SourceCompontent/Source.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./Source.css";
+
+function Source() {
+    return (
+        <div className={style.mainDiv}>
+            <div>
+                <h1>Welcome to the Dashboard</h1>
+                <p>This is the Source 1 page content.</p>
+            </div>
+        </div>
+    )
+}
+export default Source;
