fdbd4670fe8e045e44a8202e58124a6f39c5f623
IbiminaFG <gogoibimina@gmail.com>
2024-06-24 15:40:07 +0100

    updating the unpaid invoices page

--- src/page/UnpaidInvoicePage/UnpaidInvoicePage.js
+++ src/page/UnpaidInvoicePage/UnpaidInvoicePage.js
@@ -1,63 +1,69 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useLocalState } from '../../utils/useLocalStorage'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocalState } from "../../utils/useLocalStorage";
+import { Link } from "react-router-dom";
 // import table from "react-bootstrap/Table";
-import './UnpaidInvoicePage.css'
+import "./UnpaidInvoicePage.css";
 
 function UnpaidInvoicePage() {
+  const [data, setData] = useState({});
+  const [isdata, setIsdata] = useState(false);
+  const [jwt, setJwt] = useLocalState("", "jwt");
 
-  const [data, setData] = useState({})
-  const [isdata, setIsdata] = useState(false)
-  const [jwt, setJwt] = useLocalState('', 'jwt')
+  useEffect(() => {
+    axios
+      .get(
+        "https://leverpay-api.azurewebsites.net/api/v1/user/get-invoices?status=0",
+        {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data.data);
+        if (res.data.data.length > 0) {
+          setIsdata(true);
+        } else setIsdata(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsdata(false);
+      });
+  }, []);
 
-  useEffect(()=>{
-    axios.get('https://leverpay-api.azurewebsites.net/api/v1/user/get-invoices?status=pending', {
-      headers: {
-        Authorization : `Bearer ${jwt}`
-      }
-    })
-    .then(res=>{
-      console.log(res.data)
-      setData(res.data.data)
-      if(res.data.data.length > 0){
-        setIsdata(true)
-      }else setIsdata(false)
-    })
-    .catch(err=>{
-      console.log(err)
-      setIsdata(false)
-    })
-  }, [])
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toISOString().split("T")[0];
+  };
 
- 
   return (
-    <div className='Unpaid_con'>
-    <div className='Unpaid'>
-       <h1>
-            Unpaid Invoices
-        </h1>
-        <table className='table'>
-            <thead>
-                <tr>
-                <td>Date</td>
-                    <td>Name</td>
-                    <td>Amount</td>
-                    <td>Status</td>
-                    <td>Invoice</td>
-                </tr>
-            </thead>
+    <div className="Unpaid_con">
+      <div className="Unpaid">
+        <h1>Unpaid Invoices</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <td>Date</td>
+              <td>Name</td>
+              <td>Amount</td>
+              <td>Status</td>
+              <td>Invoice</td>
+            </tr>
+          </thead>
         </table>
         <table className="Unpaiddata">
-        <tbody>
-            {
-                isdata && data.map(item=>{
-                    return  <tr key={item.date} className="table-data">
-                    <td>{item.date}</td>
-                    <td>{item.name}</td>
-                    <td>{item.amt}</td>
-                    <td>{item.status}</td>
+          <tbody>
+            {isdata &&
+              data.map((item) => {
+                return (
+                  <tr key={item.uuid} className="table-data">
+                    <td>{formatDate(item.created_at)}</td>
+                    <td>{item.product_name}</td>
+                    <td>&#8358; {parseInt(parseFloat(item.price))}</td>
+                    <td>{item.status ? "Paid" : "Unpaid"}</td>
                     <td>
                       <Link
                         to="invoices/unpaid-invoice"
@@ -69,18 +75,18 @@ function UnpaidInvoicePage() {
                       </Link>
                     </td>
                   </tr>
-                })
-            }
-            {
-                !isdata && <tr style={{textAlign: 'center'}}>
-                   <td>You do not have any Pending Invoice.</td> 
-                </tr>
-            }
-        </tbody>
-      </table>
+                );
+              })}
+            {!isdata && (
+              <tr style={{ textAlign: "center" }}>
+                <td>You do not have any Pending Invoice.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default UnpaidInvoicePage
+export default UnpaidInvoicePage;
--- src/page/UserDashboardLayout.js
+++ src/page/UserDashboardLayout.js
@@ -47,7 +47,7 @@ import AllTransactionCon from "./AllTransactions/AllTransactionCon";
 import AllFundingHistoryCon from "./AllTransactions/AllFundingHistoryCon";
 import PayBillDropDown from "../page/PayBillsPage/PayBillsPage";
 import SourceComponent from "./PayBillsPage/Source1Page";
-import Source2Page  from "./PayBillsPage/Source2page";
+import Source2Page from "./PayBillsPage/Source2page";
 import CreatePinPage from "./PayBillsPage/CreatePinPage";
 
 export const UserDashboardLayout = (props) => {
@@ -80,143 +80,168 @@ export const UserDashboardLayout = (props) => {
 
   useEffect(() => {
     axios
-        .get("https://leverpay-api.azurewebsites.net/api/v1/user/get-card", {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${jwt}`,
-          },
-        })
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.error("Error fetching card data:", error);
-        });
+      .get("https://leverpay-api.azurewebsites.net/api/v1/user/get-card", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwt}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error("Error fetching card data:", error);
+      });
   }, [jwt]);
 
   return (
-      <>
-        <Routes>
-          <Route path="/investment" element={<Investment />} />
+    <>
+      <Routes>
+        <Route path="/investment" element={<Investment />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="leverpay-signup" element={<SignupPage />} />
+        <Route path="leverpay-signup/signup-OTP" element={<SignupOTP />} />
+        <Route
+          element={
+            <div className="col-md-12 flexy" style={{ overflowX: "hidden" }}>
+              <div className="nav-con">
+                <NavComponent />
+              </div>
+              <div className="col-md-10">
+                <TopNav
+                  userName={{
+                    firstName: user.first_name,
+                    lastName: user.last_name,
+                    passport: user.passport,
+                  }}
+                />
+                <PrivateRoute userName={user.first_name}>
+                  <Outlet />
+                </PrivateRoute>
+              </div>
+            </div>
+          }
+        >
           <Route
-              element={
-                <div className="col-md-12 flexy" style={{ overflowX: "hidden" }}>
-                  <div className="nav-con">
-                    <NavComponent />
+            path="/"
+            element={
+              <div className="dashboard col-md-12 flexy">
+                <div className="dashboard-right col-md-8">
+                  <div
+                    className="spending col-md-12 flexy"
+                    style={{ marginTop: "-1rem" }}
+                  >
+                    <div className="col-md-4">
+                      <TotalMoney
+                        bg="#0E093F"
+                        totaltype="Total Balance"
+                        amt={userData ? userData.wallet.amount.ngn : ""}
+                      />
+                    </div>
+                    <div className="col-md-4">
+                      <TotalMoney
+                        bg="#F6A61F"
+                        totaltype="Total Spending"
+                        amt={userData ? userData.total_spending.ngn : ""}
+                      />
+                    </div>
+                    <div className="col-md-4">
+                      <TotalMoney bg="#201E34" totaltype=" Total Saved" />
+                    </div>
                   </div>
-                  <div className="col-md-10">
-                    <TopNav
-                        userName={{
-                          firstName: user.first_name,
-                          lastName: user.last_name,
-                          passport: user.passport,
-                        }}
-                    />
-                    <PrivateRoute userName={user.first_name}>
-                      <Outlet />
-                    </PrivateRoute>
+                  <div className="statement col-md-11">
+                    <StatementComponent />
+                  </div>
+                  <div className="dashboard-transaction-table-container col-md-11">
+                    <AllTransactions />
                   </div>
                 </div>
-              }
-          >
-            <Route
-                path="/"
-                element={
-                  <div className="dashboard col-md-12 flexy">
-                    <div className="dashboard-right col-md-8">
-                      <div
-                          className="spending col-md-12 flexy"
-                          style={{ marginTop: "-1rem" }}
-                      >
-                        <div className="col-md-4">
-                          <TotalMoney
-                              bg="#0E093F"
-                              totaltype="Total Balance"
-                              amt={userData ? userData.wallet.amount.ngn : ""}
-                          />
-                        </div>
-                        <div className="col-md-4">
-                          <TotalMoney
-                              bg="#F6A61F"
-                              totaltype="Total Spending"
-                              amt={userData ? userData.total_spending.ngn : ""}
-                          />
-                        </div>
-                        <div className="col-md-4">
-                          <TotalMoney
-                              bg="#201E34"
-                              totaltype=" Total Saved"
-                          />
-                        </div>
-                      </div>
-                      <div className="statement col-md-11">
-                        <StatementComponent />
-                      </div>
-                      <div className="dashboard-transaction-table-container col-md-11">
-                        <AllTransactions />
-                      </div>
-                    </div>
-                    <div className="dashboard-left col-md-4 card-holder">
-                      <div className="col-md-12 default-card-holder">
-                        <header className="card-header">My Card</header>
-                        <CardUser />
-                      </div>
-                      <div className="mchnt-con col-md-12">
-                        <div className="mchnt-con-div">
-                          <MerchantComponent />
-                        </div>
-                      </div>
+                <div className="dashboard-left col-md-4 card-holder">
+                  <div className="col-md-12 default-card-holder">
+                    <header className="card-header">My Card</header>
+                    <CardUser />
+                  </div>
+                  <div className="mchnt-con col-md-12">
+                    <div className="mchnt-con-div">
+                      <MerchantComponent />
                     </div>
                   </div>
-                }
-            />
-            <Route path="transactions" element={<AllTransactionCon />} />
-            <Route path="funding-history" element={<AllFundingHistoryCon />} />
-            <Route exact path="transfer" element={<TransferPage />} />
-            <Route exact path="/investment" element={<Investment />} />
-            <Route path="invoices" element={<AllInvoicePage />}>
-              <Route index element={<Allinvoices />} />
-              <Route path="all-invoices" element={<Allinvoices />} />
-              <Route path="unpaid-invoices" element={<UnpaidInvoicePage />} />
-              <Route path="paid-invoice" element={<PaidInvoice />} />
-            </Route>
-            <Route path="/unpaid-invoice" element={<UnpaidReceipt />} />
-            <Route path="funding" element={<FundingPage />}>
-              <Route index element={<FundingPayment />} />
-              <Route path="stablecoins-deposit" element={<FundingPayment />} />
-              <Route path="naira-deposit" element={<FundingNaira />} />
-              <Route path="paystack-deposit" element={<FundingPaystack />} />
-              <Route path="more" element={<FundingMore />} />
-            </Route>
-            <Route path="cardCategories" element={<CardcategoryPage />} />
-            <Route path="account" element={<MyUpgradedAccount />} />
-            <Route path="my cards" element={<MyCardsSilver firstName={user.first_name} lastName={user.last_name} />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="profile" element={<ProfilePage userName={{ firstName: user.first_name, lastName: user.last_name, otherName: user.other_name, gender: user.gender, reg_email: user.email, reg_phone: user.phone }} />} />
-            <Route path="help" element={<HelpForm />} />
-            <Route path="faq" element={<Faq />} />
-            <Route path="customer-support" element={<Feedback />} />
-            <Route path="payment-page" element={<PaymentPage />}>
-              <Route index element={<CreditCard />} />
-              <Route path="credit-card" element={<CreditCard />} />
-              <Route path="checkout-transfer" element={<CheckoutTransfer isInvest={true} />} />
-            </Route>
-            <Route path="/my-subscriptions" element={<MySubscription />} />
-            <Route path="my-subscriptions/subscription-transactions" element={<SubscriptionTransactions />} />
-            <Route path="*" element={<Navigate to="/" />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="signin" element={<SignInPage />} />
-            <Route path="leverpay-signup" element={<SignupPage />} />
-            <Route path="leverpay-signup/signup-OTP" element={<SignupOTP />} />
-            <Route path="/paybills" element={<PayBillDropDown />} />
-            <Route path="/sourcePage" element={<SourceComponent />} />
-            <Route path="/PayBill/source2" element={<Source2Page />} />
-            <Route path="/PayBill/create-pin" element={<CreatePinPage />} />
+                </div>
+              </div>
+            }
+          />
+          <Route path="transactions" element={<AllTransactionCon />} />
+          <Route path="funding-history" element={<AllFundingHistoryCon />} />
+          <Route exact path="transfer" element={<TransferPage />} />
+          <Route exact path="/investment" element={<Investment />} />
+          <Route path="invoices" element={<AllInvoicePage />}>
+            <Route index element={<Allinvoices />} />
+            <Route path="all-invoices" element={<Allinvoices />} />
+            <Route path="unpaid-invoices" element={<UnpaidInvoicePage />} />
+            <Route path="paid-invoice" element={<PaidInvoice />} />
           </Route>
-        </Routes>
-        <ToastContainer />
-        <PayBillDropDown />
-      </>
+          <Route path="/unpaid-invoice" element={<UnpaidReceipt />} />
+          <Route path="funding" element={<FundingPage />}>
+            <Route index element={<FundingPayment />} />
+            <Route path="stablecoins-deposit" element={<FundingPayment />} />
+            <Route path="naira-deposit" element={<FundingNaira />} />
+            <Route path="paystack-deposit" element={<FundingPaystack />} />
+            <Route path="more" element={<FundingMore />} />
+          </Route>
+          <Route path="cardCategories" element={<CardcategoryPage />} />
+          <Route path="account" element={<MyUpgradedAccount />} />
+          <Route
+            path="my cards"
+            element={
+              <MyCardsSilver
+                firstName={user.first_name}
+                lastName={user.last_name}
+              />
+            }
+          />
+          <Route path="settings" element={<Settings />} />
+          <Route
+            path="profile"
+            element={
+              <ProfilePage
+                userName={{
+                  firstName: user.first_name,
+                  lastName: user.last_name,
+                  otherName: user.other_name,
+                  gender: user.gender,
+                  reg_email: user.email,
+                  reg_phone: user.phone,
+                }}
+              />
+            }
+          />
+          <Route path="help" element={<HelpForm />} />
+          <Route path="faq" element={<Faq />} />
+          <Route path="customer-support" element={<Feedback />} />
+          <Route path="payment-page" element={<PaymentPage />}>
+            <Route index element={<CreditCard />} />
+            <Route path="credit-card" element={<CreditCard />} />
+            <Route
+              path="checkout-transfer"
+              element={<CheckoutTransfer isInvest={true} />}
+            />
+          </Route>
+          <Route path="/my-subscriptions" element={<MySubscription />} />
+          <Route
+            path="my-subscriptions/subscription-transactions"
+            element={<SubscriptionTransactions />}
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/paybills" element={<PayBillDropDown />} />
+          <Route path="/sourcePage" element={<SourceComponent />} />
+          <Route path="/PayBill/source2" element={<Source2Page />} />
+          <Route path="/PayBill/create-pin" element={<CreatePinPage />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+      <PayBillDropDown />
+    </>
   );
 };
 
